a222a4c164ec7928416f38c412d8aa1eb3405bf0
Fredrik Vittfarne <fredrik@vittfarne.net>
2014-05-04 12:38:42 +0200

    Lite ändringar :)

--- linux/check.php
+++ linux/check.php
@@ -1,4 +1,7 @@
 <?php
+//Settings
+$renttitle = "TEH WARRiORS RENTING SYSTEM";
+
 $medlemsgrupp = 1;
 //Denna används för att jämföras med vilken grupp medlemmen är i. Om gruppen stämmer med medlemmen är medlemmen en aktiv medlem.
 
@@ -22,6 +25,8 @@ $memberhasserver = 0;
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+	<div id="wrapper">
+		<h1><?php echo $renttitle; ?></h1>
 	<?php
 
 	//Kolla om personen är medlem.
@@ -33,12 +38,33 @@ if (!$memberhasserver){
 	if (isset($_POST['pw'])) {$pwvalue = $_POST['pw'];} else {$pwvalue = "";}
 	if (isset($_POST['rcon'])) {$rconvalue = $_POST['rcon'];} else {$rconvalue = "";}
 echo <<<EOD
+	<div class="info">
 	<form action="" method="POST">
-		<input type="text" name="namn" value="{$namnvalue}" placeholder="Namn"><br>
-		<input type="text" name="pw" value="{$pwvalue}" placeholder="Serverlösenord">(används för att joina servern)<br>
-		<input type="text" name="rcon" value="{$rconvalue}" placeholder="Rconlösenord">(används för att byta bana och ändra inställningar)<br>
-		<input type="submit" value="Boka" id="boka" name="boka">
+	<table class="inputs">
+	<tr>
+		<td><label for="namn">Servernamn: </label></td><td><input class="text" type="text" id="namn" name="namn" value="{$namnvalue}" placeholder="Namn"></td>
+	</tr>
+	<tr>
+		<td><label for="pw">Lösenord: </label></td><td><input class="text" type="text" id="pw" name="pw" value="{$pwvalue}" placeholder="Serverlösenord"></td>
+	</tr>
+	<tr>
+		<td><label for="rcon">Rconlösen: </label></td><td><input class="text" type="text" id="rcon" name="rcon" value="{$rconvalue}" placeholder="Rconlösenord"></td>
+	</tr>
+	<tr>
+		<td><label for="spel">Spel: </label></td>
+		<td><select name="spel" id="spel" class="option">
+		  <option value="css" name="css">Counter Strike: Source</option> 
+		  <option value="csgo" name="csgo">Counter Strike: Global Offensive</option>
+		  <option value="cs16">Counter Strike: 1.6</option>
+		</select>
+		</td>
+	</tr>
+	<tr>
+		<td><input type="submit" value="Boka" id="boka" name="boka"></td>
+	</tr>
+		</table>
 	</form>
+	</div>
 EOD;
 } else {
 echo <<<EOD
@@ -51,6 +77,7 @@ echo <<<EOD
 			Console connect: connect 123.132.131.12:27045;password mittpw
 			Serverlösen: mittpw<br>
 			Rconlösen: mittrcon<br>
+			Spel: CSS
 		</p>
 	</div>
 	<form action="" method="POST">
@@ -75,15 +102,16 @@ EOD;
 //Först kollar vi om vi ska boka eller avboka servern eller bara visa formuläret.
 if (isset($_POST['boka'])) {
 	//Kollar om man angett fälten Namn, PW och rcon.
-	if (isset($_POST['namn']) or isset($_POST['pw']) or isset($_POST['rcon']) or $_POST['namn'] == "" or $_POST['pw'] == "" or $_POST['rcon'] == "") {
+	if (!isset($_POST['namn']) or !isset($_POST['pw']) or !isset($_POST['rcon']) or $_POST['namn'] == "" or $_POST['pw'] == "" or $_POST['rcon'] == "") {
 		//Om användaren "glömt" ange lösen, servernamn eller rcon
 		echo "Det verkar som att du glömt att ange Servernamn, Lösenord eller Rcon! Var god försök igen!";
 	} else {
 		//Laddar värden från formuläret
-		$servernamn = "TEH WARRIORS.NET" . $_POST['namn'];
+		$servernamn = "TEH WARRiORS | " . $_POST['namn'];
 		$serverlosen = $_POST['pw'];
 		$serverrcon = $_POST['rcon'];
-
+		$serverspel = $_POST['spel'];
+		echo "Name: " . $servernamn . "<br>Losen: " . $serverlosen . "<br>Rcon: " . $serverrcon . "<br>Spel: " . $serverspel;
 		//Alla teckenlängder har att göra med databasen, Tabellen innehåller 
 		if (strlen($servernamn) > 30) {
 			//Kollar om servernamet är längre än 30 tecken. (TW.NET inkluderat.)
@@ -110,5 +138,6 @@ if (isset($_POST['avboka'])) {
 
 
 ?>
+</div>
 </body>
 </html>
